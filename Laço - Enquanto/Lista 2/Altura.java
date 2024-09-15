139aff4420d75d4864adb779a1ef66143cb67f06
RhafaOli05 <rhafaoliver1805@gmail.com>
2024-09-15 20:04:54 -0300

    Add files via upload

---
+++ Laço - Enquanto/Lista 2/Altura.java
@@ -0,0 +1,13 @@
+
+public class Altura {
+	public static void main(String[] args) {
+		double p= 145, j=134;
+		int i=0;
+		while (j<p) { 
+			j=j+2.5; 
+			p=p+2;
+			i++;
+		}
+		System.out.println(i+" Anos");
+	}
+}
