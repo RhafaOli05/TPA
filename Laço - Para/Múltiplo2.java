2fab2094f823fd193437d8cab0c29b181007df16
RhafaOli05 <rhafaoliver1805@gmail.com>
2024-09-18 22:19:19 -0300

    Add files via upload

---
+++ Laço - Para/Múltiplo2.java
@@ -0,0 +1,14 @@
+
+public class Múltiplo2 {
+	public static void main(String[] args) {
+		int i;
+		for(i=1; i<=100; i++) {
+			if(i%10==0){
+				System.out.println(i+" é multiplo de 10");
+			}else {
+				System.out.println(i);
+			}
+			
+		}
+	}
+}
