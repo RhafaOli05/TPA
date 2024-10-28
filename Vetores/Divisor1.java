1d7ee9f9e3e0b7609ab9908eaffd014aede6a934
RhafaOli05 <rhafaoliver1805@gmail.com>
2024-10-28 00:46:24 -0300

    Add files via upload

---
+++ Vetores/Divisor1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Divisor1 {
+	public static void main(String[] args) {
+		Scanner leia =new Scanner(System.in);
+		int a[], i, j;
+		a = new int [10];
+		for(i=0; i<10; i++) {
+			System.out.println("Escreva o "+(i+1)+"º número do vetor A");
+			a[i]=leia.nextInt();
+			System.out.println(" ");
+		}
+		for(i=0; i<10; i++) {
+			System.out.print("A = " +a[i]+ " ");
+			System.out.print("Divisores de A: [");
+			for(j=1; j<=a[i]; j++) {
+				if(a[i]%j==0) {
+					System.out.print(j + " ");
+				}
+		    }
+			System.out.println("]\n");
+			leia.close();
+		}
+	}
+		
+}
